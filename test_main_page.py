904686c71d2b2393a1df6f3f1264312f57e1cbeb
SLaz33 <723444@gmail.com>
2022-03-02 16:46:27 +0300

    Add exception's catch during element's seurch

--- test_main_page.py
+++ test_main_page.py
@@ -11,4 +11,11 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    time.sleep(5)  #чтобы успеть посмотреть
+    time.sleep(3)  #чтобы успеть посмотреть
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    time.sleep(3)  #чтобы успеть посмотреть
